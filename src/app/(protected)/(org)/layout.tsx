1813d09455ec798d8f77200774068ac315c9c58e
Harsh Sandhu <hsandhu@speareducation.com>
2025-07-04 02:01:47 +0530

    feat: add utility container class in globals.css and apply it in OrgLayout for improved layout consistency

--- src/app/(protected)/(org)/layout.tsx
+++ src/app/(protected)/(org)/layout.tsx
@@ -17,7 +17,7 @@ const OrgLayout = ({ children }: { children: React.ReactNode }) => {
         <OrgHeader />
         <div className="flex flex-1 flex-col">
           <div className="@container/main flex flex-1 flex-col gap-2">
-            <div className="py-4 md:py-6">{children}</div>
+            <div className="container py-4 md:py-6">{children}</div>
           </div>
         </div>
       </SidebarInset>
