67991dbf226c425c54951539df4a28880084127c
Harsh Sandhu <hsandhu@speareducation.com>
2025-07-12 19:17:38 +0530

    feat(org-sidebar): Improve styling and performance of org sidebar

--- src/components/sidebars/org-sidebar.tsx
+++ src/components/sidebars/org-sidebar.tsx
@@ -2,7 +2,12 @@
 
 import * as React from "react";
 
-import { OrganizationSwitcher } from "@clerk/nextjs";
+import {
+  ClerkLoaded,
+  ClerkLoading,
+  OrganizationSwitcher,
+  UserButton,
+} from "@clerk/nextjs";
 import {
   BarChart3,
   Calendar,
@@ -11,7 +16,7 @@ import {
   User,
   Users,
 } from "lucide-react";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 
 import { CreateAppointmentModal } from "@/components/modals/CreateAppointmentModal";
 import { buttonVariants } from "@/components/ui/button";
@@ -23,12 +28,15 @@ import {
   SidebarGroupContent,
   SidebarHeader,
   SidebarMenu,
+  SidebarMenuAction,
   SidebarMenuButton,
   SidebarMenuItem,
 } from "@/components/ui/sidebar";
+import { Skeleton } from "@/components/ui/skeleton";
 import { cn } from "@/lib/utils";
 
 export function OrgSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  const pathname = usePathname();
   const router = useRouter();
 
   const navigation = [
@@ -41,60 +49,102 @@ export function OrgSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
 
   return (
     <Sidebar collapsible="offcanvas" {...props}>
-      <SidebarHeader>
-        <SidebarMenu>
-          <SidebarMenuItem>
-            <SidebarMenuButton
-              asChild
-              className="data-[slot=sidebar-menu-button]:!p-1.5"
-            >
-              <OrganizationSwitcher hidePersonal />
-            </SidebarMenuButton>
-          </SidebarMenuItem>
-        </SidebarMenu>
-      </SidebarHeader>
+      <ClerkLoading>
+        <div className="space-y-4">
+          <Skeleton className="h-8 w-full" />
+          <Skeleton className="h-80 w-full" />
+          <Skeleton className="mt-auto h-8 w-full" />
+        </div>
+      </ClerkLoading>
 
-      <SidebarContent>
-        <SidebarGroup>
-          <SidebarGroupContent className="flex flex-col gap-4">
-            <SidebarMenu>
-              <SidebarMenuItem className="flex items-center gap-2">
-                <CreateAppointmentModal>
-                  <SidebarMenuButton
-                    tooltip="Create Appointment"
-                    className={cn(
-                      buttonVariants({ variant: "outline", size: "sm" }),
-                      "w-full justify-start"
-                    )}
-                  >
-                    <PlusCircleIcon />
-                    <span>Create Appointment</span>
-                  </SidebarMenuButton>
-                </CreateAppointmentModal>
-              </SidebarMenuItem>
-            </SidebarMenu>
-            <SidebarMenu>
-              {navigation.map(item => (
-                <SidebarMenuItem key={item.name}>
-                  <SidebarMenuButton
-                    tooltip={item.name}
-                    className={cn(
-                      buttonVariants({ variant: "ghost", size: "sm" }),
-                      "w-full justify-start"
-                    )}
-                    onClick={() => router.push(item.href)}
-                  >
-                    {item.icon && <item.icon />}
-                    <span className="text-sm">{item.name}</span>
-                  </SidebarMenuButton>
+      <ClerkLoaded>
+        <SidebarHeader>
+          <SidebarMenu>
+            <SidebarMenuItem>
+              <SidebarMenuButton
+                asChild
+                className="data-[slot=sidebar-menu-button]:!p-1.5"
+              >
+                <OrganizationSwitcher
+                  hidePersonal
+                  appearance={{
+                    elements: {
+                      rootBox: "w-full",
+                      organizationSwitcherTrigger: "w-full justify-start",
+                    },
+                  }}
+                />
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarHeader>
+      </ClerkLoaded>
+
+      <ClerkLoaded>
+        <SidebarContent>
+          <SidebarGroup>
+            <SidebarGroupContent className="flex flex-col gap-4">
+              <SidebarMenu>
+                <SidebarMenuItem className="flex items-center gap-2">
+                  <CreateAppointmentModal>
+                    <SidebarMenuButton
+                      tooltip="Create Appointment"
+                      className={cn(
+                        buttonVariants({ variant: "outline", size: "sm" }),
+                        "w-full justify-start"
+                      )}
+                    >
+                      <PlusCircleIcon />
+                      <span>Create Appointment</span>
+                    </SidebarMenuButton>
+                  </CreateAppointmentModal>
                 </SidebarMenuItem>
-              ))}
-            </SidebarMenu>
-          </SidebarGroupContent>
-        </SidebarGroup>
-      </SidebarContent>
+              </SidebarMenu>
+              <SidebarMenu>
+                {navigation.map(item => (
+                  <SidebarMenuItem key={item.name}>
+                    <SidebarMenuButton
+                      tooltip={item.name}
+                      className={cn(
+                        buttonVariants({
+                          variant: pathname === item.href ? "outline" : "ghost",
+                          size: "sm",
+                        }),
+                        "w-full justify-start"
+                      )}
+                      onClick={() => router.push(item.href)}
+                    >
+                      {item.icon && <item.icon />}
+                      <span>{item.name}</span>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                ))}
+              </SidebarMenu>
+            </SidebarGroupContent>
+          </SidebarGroup>
+        </SidebarContent>
+      </ClerkLoaded>
 
-      <SidebarFooter></SidebarFooter>
+      <ClerkLoaded>
+        <SidebarFooter>
+          <SidebarMenu>
+            <SidebarMenuItem>
+              <SidebarMenuAction asChild>
+                <UserButton
+                  showName
+                  appearance={{
+                    elements: {
+                      rootBox: "w-full",
+                      userButtonTrigger: "w-full justify-start",
+                      userButtonBox: "flex-row-reverse",
+                    },
+                  }}
+                />
+              </SidebarMenuAction>
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarFooter>
+      </ClerkLoaded>
     </Sidebar>
   );
 }
